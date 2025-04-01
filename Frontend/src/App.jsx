f103442c932f078130504789406acd919a4bbe25
Andresmahu <andres.m.hurtado2907@gmail.com>
2025-03-31 21:58:24 -0500

    ultima modificacion

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
+import Login from "./components/login";
 import MovieList from "./components/MovieList";
 import MovieDetail from "./components/MovieDetail";
 import RentalMovie from "./components/RentMovie"
